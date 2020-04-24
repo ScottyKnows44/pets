06c909df8db4f98c1addd27f257aeda0bbaa19bc
Artem Vityuk <avityuk@mail.greenriver.edu>
2020-04-24 16:25:33 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require ("vendor/autoload.php");
+
+// Instantiate the F3 base class
+$f3 = Base::instance();
+
+// Default route
+$f3->route('GET/', function() {
+
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+    echo "<p>pet</p>";
+});
+
+// Run F3
+$f3->run();
