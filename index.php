0ad1929988529b3bbd2368367c440785b7352c8c
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-04-24 16:32:42 -0400

    fixed get

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Require the autoload file
-require ("vendor/autoload.php");
+require_once('vendor/autoload.php');
 
 // Instantiate the F3 base class
 $f3 = Base::instance();
 
 // Default route
-$f3->route('GET/', function() {
+$f3->route('GET / ', function() {
 
     //$view = new Template();
     //echo $view->render('views/home.html');
